101a0ba78fe030cdf23849d8d512d893c8ae2041
Raymone Miller <raymone.c.miller.mil@army.mil>
2024-03-15 15:02:04 -0400

    Update tests

--- tests/queue_tests.c
+++ tests/queue_tests.c
@@ -3,15 +3,15 @@
 #include <CUnit/CUnit.h>
 #include <stdlib.h>
 
-enum {
-    SUCCESS = 0,
-    CAPACITY = 5,
-};
-
 // The queue to be used by all the tests
 queue_t *queue = NULL;
+
 // The integer all node data[5] pointers point to
 int data[] = {1, 2, 3, 4, 5};
+
+#define CAPACITY sizeof(data) / sizeof(*data)
+#define SUCCESS 0
+#define INVALID_QUEUE NULL
 
 int init_suite1(void) { return 0; }
 
@@ -22,7 +22,7 @@ int clean_suite1(void) { return 0; }
  *
  * @param mem_addr pointer of the item to be free'd
  */
-void custom_free(void *mem_addr) {}
+void custom_free(void *mem_addr) { (void)mem_addr; }
 
 int test_compare_node(const void *value_to_find, const void *node_data) {
     int value_to_find_int = value_to_find == NULL ? -1 : *(int *)value_to_find;
@@ -46,40 +46,32 @@ void test_queue_init() {
 }
 
 void test_queue_enqueue() {
-    queue_t *invalid_queue = NULL;
-
     // Should catch if enqueue is called on an invalid queue or with invalid
     // data
-    int exit_code = queue_enqueue(invalid_queue, &data[0]);
-    CU_ASSERT_NOT_EQUAL(exit_code, SUCCESS);
+    CU_ASSERT_NOT_EQUAL(queue_enqueue(INVALID_QUEUE, &data[0]), SUCCESS);
 
     CU_ASSERT_PTR_NOT_NULL_FATAL(queue);
     // enqueue CAPACITY number of nodes
     for (size_t i = 0; i < CAPACITY; i++) {
-        exit_code = queue_enqueue(queue, &data[i]);
         // Function exited correctly
-        CU_ASSERT_EQUAL(exit_code, SUCCESS);
+        CU_ASSERT_EQUAL(queue_enqueue(queue, &data[i]), SUCCESS);
     }
 
     // queue size is correct
     CU_ASSERT_EQUAL(queue_size(queue), CAPACITY);
 
     // Function should return a code if enqueue is called on a full queue
-    exit_code = queue_enqueue(queue, &data[5]);
-    CU_ASSERT_NOT_EQUAL(exit_code, SUCCESS);
+    CU_ASSERT_NOT_EQUAL(queue_enqueue(queue, &data[5]), SUCCESS);
 }
 
 void test_queue_dequeue() {
-    queue_t *invalid_queue = NULL;
-
     // Should catch if enqueue is called on an invalid queue
-    void *node = queue_dequeue(invalid_queue);
-    CU_ASSERT_PTR_NULL(node);
+    CU_ASSERT_PTR_NULL(queue_dequeue(INVALID_QUEUE));
 
     CU_ASSERT_PTR_NOT_NULL_FATAL(queue);
     // Dequeue all items
     for (size_t i = 0; queue_size(queue) > 0; i++) {
-        node = queue_dequeue(queue);
+        void *node = queue_dequeue(queue);
         CU_ASSERT_PTR_NOT_NULL_FATAL(node);
         CU_ASSERT_PTR_EQUAL(node, &data[i]);
     }
@@ -88,7 +80,7 @@ void test_queue_dequeue() {
     queue_enqueue(queue, &data[0]);
     queue_enqueue(queue, &data[1]);
 
-    node = queue_dequeue(queue);
+    void *node = queue_dequeue(queue);
     CU_ASSERT_PTR_NOT_NULL_FATAL(node);
     CU_ASSERT_EQUAL(*(int *)node, data[0]);
 
@@ -113,23 +105,18 @@ void test_queue_dequeue() {
 }
 
 void test_queue_peek() {
-    queue_t *invalid_queue = NULL;
-
     // Should catch if peek is called on an invalid queue or empty
-    void *node = queue_peek(invalid_queue);
-    CU_ASSERT_PTR_NULL(node);
+    CU_ASSERT_PTR_NULL(queue_peek(INVALID_QUEUE));
 
     CU_ASSERT_PTR_NOT_NULL_FATAL(queue);
-    node = queue_peek(queue);
-    CU_ASSERT_PTR_NULL(node); // queue is empty
+    CU_ASSERT_PTR_NULL(queue_peek(queue)); // queue is empty
 
     // enqueue CAPACITY number of nodes
     for (size_t i = 0; i < CAPACITY; i++) {
         queue_enqueue(queue, &data[i]);
     }
 
-    node = queue_peek(queue);
-
+    void *node = queue_peek(queue);
     // Function should have exited successfully
     CU_ASSERT_PTR_NOT_NULL_FATAL(node);
     // Correct value should have been peeked from front node
@@ -139,21 +126,17 @@ void test_queue_peek() {
 }
 
 void test_queue_get() {
-    queue_t *invalid_queue = NULL;
-
     // Should catch if get is called on an invalid queue
-    void *node = queue_get(invalid_queue, 0);
-    CU_ASSERT_PTR_NULL(node);
+    CU_ASSERT_PTR_NULL(queue_get(INVALID_QUEUE, 0));
 
     CU_ASSERT_PTR_NOT_NULL_FATAL(queue);
     // Should catch if get is called outside of the queue's bounds
-    node = queue_get(queue, CAPACITY + 1);
-    CU_ASSERT_PTR_NULL(node);
+    CU_ASSERT_PTR_NULL(queue_get(queue, CAPACITY + 1));
 
     // check random positions in the queue
     size_t indexes[] = {0, 3, 1, 4, 2};
     for (size_t i = 0; i < CAPACITY; i++) {
-        node = queue_get(queue, indexes[i]);
+        void *node = queue_get(queue, indexes[i]);
         CU_ASSERT_PTR_NOT_NULL_FATAL(node);
         CU_ASSERT_EQUAL(*(int *)node, data[indexes[i]]);
     }
@@ -163,21 +146,19 @@ void test_queue_get() {
 }
 
 void test_queue_find_first() {
-    queue_t *invalid_queue = NULL;
-
     // Should catch if find is called on an invalid queue
-    void *node = queue_find_first(invalid_queue, NULL, NULL);
-    CU_ASSERT_PTR_NULL(node);
+    int err = SUCCESS;
+    CU_ASSERT_PTR_NULL(queue_find_first(INVALID_QUEUE, NULL, &err));
+    CU_ASSERT_EQUAL(err, EINVAL);
 
     CU_ASSERT_PTR_NOT_NULL_FATAL(queue);
     // Should catch if find is called on NULL data
-    node = queue_find_first(queue, NULL, NULL);
-    CU_ASSERT_PTR_NULL(node);
+    CU_ASSERT_PTR_NULL(queue_find_first(queue, NULL, NULL));
 
     // check random positions in the queue
     size_t indexes[] = {0, 3, 1, 4, 2};
     for (size_t i = 0; i < CAPACITY; i++) {
-        node = queue_find_first(queue, &data[indexes[i]], NULL);
+        void *node = queue_find_first(queue, &data[indexes[i]], NULL);
         CU_ASSERT_PTR_NOT_NULL_FATAL(node);
         CU_ASSERT_EQUAL(*(int *)node, data[indexes[i]]);
     }
@@ -187,25 +168,24 @@ void test_queue_find_first() {
 }
 
 void test_queue_remove() {
-    queue_t *invalid_queue = NULL;
-
     // Should catch if find is called on an invalid queue
-    void *node = queue_remove(invalid_queue, NULL, NULL);
-    CU_ASSERT_PTR_NULL(node);
+    int err = SUCCESS;
+    CU_ASSERT_PTR_NULL(queue_remove(INVALID_QUEUE, NULL, &err));
+    CU_ASSERT_EQUAL(err, EINVAL);
 
     CU_ASSERT_PTR_NOT_NULL_FATAL(queue);
     // Should catch if remove is called on NULL data
-    node = queue_remove(queue, NULL, NULL);
-    CU_ASSERT_PTR_NULL(node);
+    CU_ASSERT_PTR_NULL(queue_remove(queue, NULL, NULL));
 
     // check random positions in the queue
     size_t indexes[] = {0, 3, 1, 4, 2};
     for (size_t i = 0; i < CAPACITY; i++) {
-        node = queue_remove(queue, &data[indexes[i]], NULL);
+        int *num_ptr = &data[indexes[i]];
+        void *node = queue_remove(queue, num_ptr, NULL);
         CU_ASSERT_PTR_NOT_NULL_FATAL(node);
-        CU_ASSERT_EQUAL(*(int *)node, data[indexes[i]]);
+        CU_ASSERT_EQUAL(*(int *)node, *num_ptr);
         // confirm removal
-        node = queue_find_first(queue, &data[indexes[i]], NULL);
+        node = queue_find_first(queue, num_ptr, NULL);
         CU_ASSERT_PTR_NULL(node);
     }
 
@@ -216,36 +196,27 @@ void test_queue_remove() {
 }
 
 void test_queue_clear() {
-    queue_t *invalid_queue = NULL;
-
     // Should catch if clear is called on an invalid queue
-    int exit_code = queue_clear(invalid_queue);
-    CU_ASSERT_NOT_EQUAL(exit_code, SUCCESS);
+    CU_ASSERT_EQUAL(queue_clear(INVALID_QUEUE), EINVAL);
 
     CU_ASSERT_PTR_NOT_NULL_FATAL(queue);
-    exit_code = queue_clear(queue);
     // Function should have exited fully
-    CU_ASSERT_EQUAL(exit_code, SUCCESS);
+    CU_ASSERT_EQUAL(queue_clear(queue), SUCCESS);
     // queue should now be empty
     CU_ASSERT_EQUAL(queue_size(queue), 0);
 }
 
 void test_queue_destroy() {
-    queue_t *invalid_queue = NULL;
-
     // Should catch if delete is called on an invalid queue
-    int exit_code = queue_destroy(&invalid_queue);
-    CU_ASSERT_NOT_EQUAL(exit_code, SUCCESS);
+    CU_ASSERT_EQUAL(queue_destroy(INVALID_QUEUE), EINVAL);
 
     CU_ASSERT_PTR_NOT_NULL_FATAL(queue);
     // Function should have exited fully
-    exit_code = queue_destroy(&queue);
-    CU_ASSERT_EQUAL(exit_code, SUCCESS);
+    CU_ASSERT_EQUAL(queue_destroy(&queue), SUCCESS);
 
     // Should catch if delete is called on the queue that has already been
     // deleted
-    exit_code = queue_destroy(&queue);
-    CU_ASSERT_NOT_EQUAL(exit_code, SUCCESS);
+    CU_ASSERT_EQUAL(queue_destroy(&queue), EINVAL);
 }
 
 int main(void) {
