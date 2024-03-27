3be2fc429f08d459f0f7e617cf425cfa1fd68a51
Raymone Miller <raymone.c.miller.mil@army.mil>
2024-03-27 15:11:20 -0400

    Read a fixed amount of data from a file descriptor

--- include/serialization.h
+++ include/serialization.h
@@ -1,4 +1,23 @@
 #ifndef SERIALIZATION_H
 #define SERIALIZATION_H
 
+#include <stddef.h>
+
+/* FUNCTIONS */
+
+/**
+ * @brief Read a fixed amount of data from a file descriptor.
+ *
+ * If an error occurs, a non-zero error code will be returned
+ * Possible errors:
+ *   ENODATA: - the file descriptor has reached the end of the file early
+ * See read(2) for additional possible errors.
+ *
+ * @param fd - the file descriptor
+ * @param buff - pointer to store the data
+ * @param read_sz - the size of the data to read
+ * @return int - 0 on success, non-zero on failure
+ */
+int read_exact(int fd, void *buff, size_t read_sz);
+
 #endif /* SERIALIZATION_H */
--- src/serialization.c
+++ src/serialization.c
@@ -1,1 +1,27 @@
 #include "serialization.h"
+#include <errno.h>
+#include <stdint.h>
+#include <unistd.h>
+
+/* DATA */
+
+#define SUCCESS 0
+
+/* PUBLIC FUNCTIONS*/
+
+int read_exact(int fd, void *buff, size_t read_sz) {
+    uint8_t *buf_ptr = (uint8_t *)buff;
+    size_t total_len = 0;
+    ssize_t bytes_read;
+    do {
+        if ((bytes_read = read(fd, buf_ptr, read_sz)) < 0) {
+            return errno;
+        } else if (bytes_read == 0) {
+            return ENODATA;
+        }
+        total_len += bytes_read;
+        buf_ptr += bytes_read;
+        read_sz -= bytes_read;
+    } while (total_len < read_sz);
+    return SUCCESS;
+}
