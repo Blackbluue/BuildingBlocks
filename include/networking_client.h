94ffb851d0c1503fd03f48ffb58af6235b7265d7
Raymone Miller <raymone.c.miller.mil@army.mil>
2024-04-02 09:39:52 -0400

    Open client side port

--- include/networking_client.h
+++ include/networking_client.h
@@ -1,4 +1,20 @@
 #ifndef NETWORKING_CLIENT_H
 #define NETWORKING_CLIENT_H
 
+#include "networking_utils.h"
+
+/* DATA */
+
+enum err_type {
+    SYS,  // system error
+    GAI,  // getaddrinfo error
+    SOCK, // socket error
+    CONN, // connect error
+};
+
+/* FUNCTIONS */
+
+int get_server_sock(const char *host, const char *port,
+                    const networking_attr_t *attr, int *err, int *err_type);
+
 #endif /* NETWORKING_CLIENT_H */
