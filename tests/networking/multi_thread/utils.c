29406b083f92ca9e68b16e7b44339cddf3326ab4
Raymone Miller <raymone.c.miller.mil@army.mil>
2024-05-01 13:05:11 -0400

    Print client address

--- tests/networking/multi_thread/utils.c
+++ tests/networking/multi_thread/utils.c
@@ -1,5 +1,8 @@
+#define _DEFAULT_SOURCE
 #include "utils.h"
 #include "buildingblocks.h"
+#include <netdb.h>
+#include <stdio.h>
 
 /* DATA */
 
@@ -12,8 +15,12 @@
 int send_response(struct packet *pkt, struct sockaddr *addr, socklen_t addrlen,
                   int client_sock) {
     (void)pkt;
-    (void)addr;
-    (void)addrlen;
     (void)client_sock;
+    char host[NI_MAXHOST];
+    char serv[NI_MAXSERV];
+    if (getnameinfo(addr, addrlen, host, sizeof(host), serv, sizeof(serv),
+                    NO_FLAGS) == SUCCESS) {
+        printf("host=%s, serv=%s\n", host, serv);
+    }
     return SUCCESS;
 }
