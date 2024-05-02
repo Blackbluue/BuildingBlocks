a6a15ea73e8ee0a8592e3eb1efe55d69c532fa6a
Raymone Miller <raymone.c.miller.mil@army.mil>
2024-05-02 09:50:05 -0400

    Repeat letters test complete

--- tests/networking/multi_thread/utils.c
+++ tests/networking/multi_thread/utils.c
@@ -3,6 +3,7 @@
 #include "buildingblocks.h"
 #include <netdb.h>
 #include <stdio.h>
+#include <string.h>
 
 /* DATA */
 
@@ -10,17 +11,34 @@
 
 /* PRIVATE FUNCTIONS */
 
+static int count_letters(const struct packet *pkt, int sock) {
+    (void)pkt;
+    // struct counter_packet *req = pkt->data;
+    return write_pkt_data(sock, NULL, 0, SVR_FAILURE);
+}
+
+static int repeat_letters(const struct packet *pkt, int sock) {
+    struct counter_packet *req = pkt->data;
+    memset(req->string, req->character, req->count);
+    return write_pkt_data(sock, req, sizeof(*req), SVR_SUCCESS);
+}
+
 /* PUBLIC FUNCTIONS */
 
 int send_response(struct packet *pkt, struct sockaddr *addr, socklen_t addrlen,
                   int client_sock) {
-    (void)pkt;
-    (void)client_sock;
     char host[NI_MAXHOST];
     char serv[NI_MAXSERV];
     if (getnameinfo(addr, addrlen, host, sizeof(host), serv, sizeof(serv),
                     NO_FLAGS) == SUCCESS) {
-        printf("host=%s, serv=%s\n", host, serv);
+        fprintf(stderr, "host=%s, serv=%s\n", host, serv);
     }
-    return SUCCESS;
+    switch (pkt->hdr->data_type) {
+    case RQU_COUNT:
+        return count_letters(pkt, client_sock);
+    case RQU_REPEAT:
+        return repeat_letters(pkt, client_sock);
+    default:
+        return write_pkt_data(client_sock, NULL, 0, SVR_INVALID);
+    }
 }
