0b76ad57ebabba68e35029cb20911c08ff9fd173
Raymone Miller <raymone.c.miller.mil@army.mil>
2024-05-30 13:05:02 -0400

    Simplify socket creations

--- src/python/serialization.py
+++ src/python/serialization.py
@@ -64,9 +64,7 @@ class IO_Info:
 
     _header_packer = Struct("!3I")
 
-    def __init__(
-        self, host: Union[bytes, str, None], port: Union[bytes, str, int, None]
-    ) -> None:
+    def __init__(self, host: Union[str, None], port: int) -> None:
         """Create a new instance of IO_Info.
 
         Args:
@@ -76,47 +74,16 @@ class IO_Info:
         Raises:
             OSError: If the socket could not be opened and connected to.
         """
-        self._connect_sock(host, port)
+        try:
+            self._sock = socket.create_connection((host, port))
+        except OSError as exc:
+            self._sock = None
+            raise OSError("Could not open or connect to socket") from exc
 
     def __del__(self) -> None:
         if self._sock is not None:
             self._sock.close()
             self._sock = None
-
-    def _connect_sock(
-        self, host: Union[bytes, str, None], port: Union[bytes, str, int, None]
-    ) -> None:
-        """Connect to the server.
-
-        Args:
-            host (bytes | str | None): The host to connect to. Use None for localhost.
-            port (bytes | str | int | None): The port to connect to.
-
-        Raises:
-            err: If the socket could not be opened and connected to.
-        """
-        sock_ok = False
-        for res in socket.getaddrinfo(
-            host, port, socket.AF_UNSPEC, socket.SOCK_STREAM
-        ):
-            addr_fam, socktype, proto, _, sock_addr = res
-            try:
-                self._sock = socket.socket(addr_fam, socktype, proto)
-            except OSError as exc:
-                err = exc
-                continue
-            try:
-                self._sock.connect(sock_addr)
-            except OSError as exc:
-                self._sock.close()
-                err = exc
-                continue
-            sock_ok = True
-            break
-        if not sock_ok:
-            print("Could not open or connect to socket", file=sys.stderr)
-            if err:
-                raise err
 
     def write_pkt_data(self, data: bytes, len: int, data_type: int) -> None:
         """Write packet data to the socket.
